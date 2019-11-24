9eef7f766c83f8ff203fb17e5966bc59d32482a4
Fernando Ama <fernandoama95@outlook.es>
2019-11-23 21:06:53 -0600

    Inicializando proyecto
    Inicializando proyecto

---
+++ src/main/java/com/prueba/patrones/PatternsTests/controller/Controller.java
@@ -0,0 +1,9 @@
+package com.prueba.patrones.PatternsTests.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/prueba-patrones/")
+public class Controller {
+}
---
+++ src/test/java/com/prueba/patrones/PatternsTests/PatternsTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.patrones.PatternsTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternsTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
